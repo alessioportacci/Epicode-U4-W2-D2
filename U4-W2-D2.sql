e7a2e0368a9c087cc7e73640797dd96d370cb563
Alessio <134380049+alessioportacci@users.noreply.github.com>
2023-09-05 11:50:12 +0200

    Aggiunte query

---
+++ U4-W2-D2.sql
@@ -0,0 +1,40 @@
+--1)
+--SELECT *
+--FROM dbo.Products
+
+--2)
+--SELECT * 
+--FROM dbo.Products
+--WHERE dbo.Products.UnitsInStock > 39
+
+--3)
+--SELECT *
+--FROM dbo.Customers
+--WHERE dbo.Customers.City = 'London'
+
+--4)
+--SELECT *
+--FROM  dbo.Orders
+--ORDER BY dbo.Orders.Freight DESC
+
+--5)
+--SELECT *
+--FROM  dbo.Orders
+--WHERE dbo.Orders.Freight > 89
+--	AND dbo.Orders.Freight < 201
+
+--6)
+--SELECT * 
+--FROM dbo.Products
+--WHERE dbo.Products.CategoryId = 1
+
+--7)
+--SELECT *
+--FROM dbo.[Order Details]
+--WHERE dbo.[Order Details].Discount > 0
+
+--8)
+--SELECT *
+--FROM dbo.Orders
+--WHERE dbo.Orders.CustomerID = 'BOTTM'
+--	AND dbo.Orders.Freight > 50
